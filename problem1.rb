ec2490277577fda141422e82787f201e9b90133c
Brian Skinner <brian.skinner9@gmail.com>
2012-07-23 21:47:16 -0700

    a new update

--- problem1.rb
+++ problem1.rb
@@ -2,7 +2,7 @@ def palindrom(string)
 
 	include Enumerable
 
-	result = string.lines.select { |s| s =~ /\w/}
+	result = string.lines.map { |s| s =~ /\w/}
 	#.
 	#	map { |s| s.downcase }
 		#downcase ==
