afae009b011bddc74c41cec7ceba1ec350a4532d
Brian Skinner <brian.skinner9@gmail.com>
2012-07-23 21:08:08 -0700

    some code

--- problem1.rb
+++ problem1.rb
@@ -1,3 +1,19 @@
 def palindrom?(string)
 
+	result = string.select(\w).
+		downcase ==
+		string.select(\w).
+		downcase.
+		reverse
+
 end
+
+
+
+# \w is "word char"
+# \W is "non word char"
+
+# \b is start of string
+# \B is end of string
+
+# \i is case-insensitive
