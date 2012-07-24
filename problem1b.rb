b639b3192a08d17f5de2d68c3a8de9a4d3da8068
Brian Skinner <brian.skinner9@gmail.com>
2012-07-23 23:03:44 -0700

    a new update

--- problem1b.rb
+++ problem1b.rb
@@ -4,7 +4,9 @@ def count_words(string)
 	
 	allWordsWithDups = string.lines.map { |s| s.downcase.split(/\W+/) }[0]
 	
-	string.lines.map { |s| s.downcase.split(/\W+/) }[0].uniq
-	
-	
+	allWordWithCounts = Hash.new
+	string.lines.map { |s| s.downcase.split(/\W+/) }[0].uniq.
+		each do |word|
+			allWordWithCounts(|word|) = allWordsWithDups.count(|word|)
+			
 end
