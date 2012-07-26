5a86769eef219b35d8d8271713c4c8606a9df922
Brian Skinner <brian.skinner9@gmail.com>
2012-07-26 00:44:18 -0700

    some message

--- problem3.rb
+++ problem3.rb
@@ -6,14 +6,12 @@ def combine_anagrams(words)
 	words.map do |word| 
 		sortedHash[word] = word.downcase.each_char.to_a.sort.join
 	end
-	#sortedHash.invert
-	
-	puts "sortedHash"
-	puts sortedHash
 	
 	result = Array.new
 	sortedHash.each_value.to_a.uniq.each do |sorted|
 		result.push(sortedHash.select {|k,v| v == sorted}.each_key.to_a)
+		
+		puts sortedHash.select {|k,v| v == sorted}.each_key.to_a
 	end
 	puts "result"
 	puts result
