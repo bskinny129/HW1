28ef4141ce5b96643a8d5a43eae611de277d561d
Brian Skinner <brian.skinner9@gmail.com>
2012-07-26 00:13:09 -0700

    some message

--- problem3.rb
+++ problem3.rb
@@ -5,9 +5,9 @@ def combine_anagrams(words)
 	allUniques = words.map { |word| word.downcase.each_char.to_a.sort.join }.
 		uniq
 
-#	allUniques.each do |uniq|
-		
-#	end
+	allUniques.each do |uniq|
+		words.select { |word| word.each_char.to_a.sort.join =~ /^#{uniq}$/i }
+	end
 	
 #	words.map { |word| puts word.lines.sort }
 #		puts word.downcase.map { |c| c.
