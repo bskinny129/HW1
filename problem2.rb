ec013f4ea50d5353cc46499fb485624389b46482
Brian Skinner <brian.skinner9@gmail.com>
2012-07-25 21:47:27 -0700

    some message

--- problem2.rb
+++ problem2.rb
@@ -2,12 +2,25 @@ class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 include Enumerable
 
+class String 
+	def <=>(str)
+		if self == str return 0
+		if self == "s" && str == "p" return 1
+		if self > str
+			return -1
+		else
+			return 1
+		end
+		
+	end
+end
+
 def rps_game_winner(game)
 	raise WrongNumberOfPlayersError unless game.length == 2
 
 	raise NoSuchStrategyError unless game.select { |player| player[1] =~ /^(r|p|s)$/i }.
 		length == 2
 	
-	game.sort { |a,b| a[1].downcase<=>b[1].downcase }
+	game.sort { |a,b| a[1].downcase<=>b[1].downcase }[0]
 		
 end
