c188b30010606f86e20f26ba25ab05044aa41c4b
Brian Skinner <brian.skinner9@gmail.com>
2012-07-25 23:51:53 -0700

    some message

--- problem3.rb
+++ problem3.rb
@@ -2,8 +2,9 @@
 
 def combine_anagrams(words)
 
+	words[0].sort
 
-	words.map { |word| puts word.lines.sort }
+#	words.map { |word| puts word.lines.sort }
 #		puts word.downcase.map { |c| c.
 #	end
 
