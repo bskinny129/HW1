53972dce38bafdb1fbd4ad4a110566c8ac4acb50
Brian Skinner <brian.skinner9@gmail.com>
2012-07-26 00:30:42 -0700

    some message

--- problem3.rb
+++ problem3.rb
@@ -8,7 +8,7 @@ def combine_anagrams(words)
 	end
 	sortedHash.invert
 	
-	sortedHash.each_value
+	sortedHash.each_value.to_a.uniq
 	#sortedHash.select {|k,v| v = }
 	#allUniques.each do |uniq|
 	#	words.select { |word| word.each_char.to_a.sort.join  }
