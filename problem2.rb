87376ae8d86263f4c914bb8b2ea8941b177ecc68
Brian Skinner <brian.skinner9@gmail.com>
2012-07-25 21:14:26 -0700

    some message

--- problem2.rb
+++ problem2.rb
@@ -6,6 +6,6 @@ def rps_game_winner(game)
 raise WrongNumberOfPlayersError unless game.length == 2
 # your code here
 
-	game.select { |player| player[1] ~= /^(r|p|s)$/i }
+	game.select { |player| player[1] =~ /^(r|p|s)$/i }
 
 end
