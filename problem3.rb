669a018f2c2a28674fd0979c8bf720841ad37ae6
Brian Skinner <brian.skinner9@gmail.com>
2012-07-26 00:21:50 -0700

    some message

--- problem3.rb
+++ problem3.rb
@@ -2,12 +2,15 @@
 
 def combine_anagrams(words)
 
-	allUniques = words.map { |word| word.downcase.each_char.to_a.sort.join }.
-		uniq
-
-	allUniques.each do |uniq|
-		words.select { |word| puts word.each_char.to_a.sort.join  }
+	
+	sortedHash = Hash.new
+	words.map do |word| 
+		sortedHash[word] = word.downcase.each_char.to_a.sort.join
 	end
+	puts sortedHash
+	#allUniques.each do |uniq|
+		words.select { |word| word.each_char.to_a.sort.join  }
+	#end
 	
 #	=~ /^#{uniq}$/i
 
