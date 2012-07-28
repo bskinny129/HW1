1f15dd9bc8dfa9beb25a0707e4b6b48d55743b8f
Brian Skinner <brian.skinner9@gmail.com>
2012-07-28 15:11:08 -0700

    some commit

---
+++ problem5a.rb
@@ -0,0 +1,12 @@
+class Numeric
+	@@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+	
+	def method_missing(method_id)
+		singular_currency = method_id.to_s.gsub( /s$/, '')
+		if @@currencies.has_key?(singular_currency)
+			self * @@currencies[singular_currency]
+		else
+			super
+		end
+	end
+end
