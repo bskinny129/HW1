177fce9ceacb9068c5ccea76bf2b00fb446311ef
Brian Skinner <brian.skinner9@gmail.com>
2012-07-28 15:47:00 -0700

    some commit

--- problem5a.rb
+++ problem5a.rb
@@ -1,13 +1,17 @@
+class NoSuchCurrencyError < StandardError ; end
+
 class Numeric
-	@@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+	@@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1}
 	
 	def dollar; self; end
-#	def euros; self * @@currencies['euro']; end
-#	def rupees; self * @@currencies['rupee']; end
-#	def yens; self * @@currencies['yen']; end
 	
 	def in(currency)
-		self * @@currencies[currency.to_s.gsub( /s$/, '')]
+		singular_currency = currency.to_s.gsub( /s$/, '')
+		if @@currencies.has_key?(singular_currency)
+			self * @@currencies[singular_currency]
+		else
+			raise WrongNumberOfPlayersError
+		end
 	end
 	
 	def method_missing(method_id)
