cded4be60405abb2d6aa4812b7eda54f693420f7
Brian Skinner <brian.skinner9@gmail.com>
2012-07-28 15:41:38 -0700

    some commit

--- problem5a.rb
+++ problem5a.rb
@@ -13,6 +13,7 @@ class Numeric
 	def method_missing(method_id)
 
 		singular_currency = method_id.to_s.gsub( /s$/, '')
+		puts "singular " + singular_currency
 		if @@currencies.has_key?(singular_currency)
 			self * @@currencies[singular_currency]
 		elsif singular_currency == dollar
