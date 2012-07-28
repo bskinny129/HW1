79d9e15c554da145ab0cf0e00913c27a22aa2241
Brian Skinner <brian.skinner9@gmail.com>
2012-07-28 15:39:13 -0700

    some commit

--- problem5a.rb
+++ problem5a.rb
@@ -2,20 +2,24 @@ class Numeric
 	@@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
 	
 	def dollars; self; end
-	def euros; self * @@currencies[euro]; end
-	def rupees; self * @@currencies[rupee]; end
-	def yens; self * @@currencies[yen]; end
+#	def euros; self * @@currencies['euro']; end
+#	def rupees; self * @@currencies['rupee']; end
+#	def yens; self * @@currencies['yen']; end
 	
 	def in(currency)
 		self * @@currencies[currency.to_s.gsub( /s$/, '')]
 	end
 	
 	def method_missing(method_id)
+
 		singular_currency = method_id.to_s.gsub( /s$/, '')
 		if @@currencies.has_key?(singular_currency)
 			self * @@currencies[singular_currency]
+		elsif singular_currency == dollar
+			self
 		else
 			super
 		end
+
 	end
 end
