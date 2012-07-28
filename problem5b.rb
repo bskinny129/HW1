e7f11a7ae797dfff6a4defda0f579d59f53cf757
Brian Skinner <brian.skinner9@gmail.com>
2012-07-28 16:19:14 -0700

    some commit

--- problem5b.rb
+++ problem5b.rb
@@ -5,12 +5,13 @@ class String
 		palindrome?(self)
 	end
 	
-	def palindrome?(string)
-		include Enumerable
-
-		string.lines.map { |s| s.gsub(/\W/,'').reverse.downcase } == 
-			string.lines.map { |s| s.gsub(/\W/,'').downcase }
-
-	end
 end
 
+module Enumerable
+	def palindrome?(string)
+			include Enumerable
+
+			string.lines.map { |s| s.gsub(/\W/,'').reverse.downcase } == 
+				string.lines.map { |s| s.gsub(/\W/,'').downcase }
+	end
+end
