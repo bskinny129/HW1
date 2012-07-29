013872f070d4411e871497c3e80fb4623ace3448
Brian Skinner <brian.skinner9@gmail.com>
2012-07-28 18:18:20 -0700

    some message

--- problem6.rb
+++ problem6.rb
@@ -13,13 +13,13 @@ class CartesianProduct
 end	
 	
 #Examples of use
-c = CartesianProduct.new([:a,:b], [4,5])
-c.each { |elt| puts elt.inspect }
+#c = CartesianProduct.new([:a,:b], [4,5])
+#c.each { |elt| puts elt.inspect }
 # [:a, 4]
 # [:a, 5]
 # [:b, 4]
 # [:b, 5]
-c = CartesianProduct.new([:a,:b], [])
-c.each { |elt| puts elt.inspect }
+#c = CartesianProduct.new([:a,:b], [])
+#c.each { |elt| puts elt.inspect }
 # (nothing printed since Cartesian product
 # of anything with an empty collection is empty)
